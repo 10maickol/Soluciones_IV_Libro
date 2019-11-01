6b1ebecad58b61964394d0decb12dbaa7152ee64
maickol <maickol_sm@outlook.com>
2019-10-31 21:13:55 -0500

    formulario nuevo libro cambios

--- libro/libro/NuevoLibrocs.Designer.cs
+++ libro/libro/NuevoLibrocs.Designer.cs
@@ -28,11 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtemisor = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Orden:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtemisor
+            // 
+            this.txtemisor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemisor.Location = new System.Drawing.Point(102, 26);
+            this.txtemisor.Name = "txtemisor";
+            this.txtemisor.Size = new System.Drawing.Size(157, 26);
+            this.txtemisor.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Generalidades",
+            "Ciencias Sociales",
+            "Ciencias Naturales y Matematicas",
+            "Tecnologia (Ciencias Aplicadas)",
+            "Las artes.  Bellas artes y artes decorativas",
+            "Literatura y retórica",
+            "Geografía e historia",
+            "Lenguas",
+            "Ciencias naturales y matemáticas",
+            "Filosofía y Psicología"});
+            this.comboBox1.Location = new System.Drawing.Point(371, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(188, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(283, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Principal:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Sub Nivel 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(367, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Sub Nivel 2:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Generalidades",
+            "Bibliografía",
+            "Bibliotecología y ciencias de la información",
+            "Psicología",
+            "Ética (Filosofía moral)",
+            "Filosofía antigua , medieval, oriental",
+            "Religión",
+            "Cristianismo Teología Cristiana",
+            "Moral cristiana y teología piadosa",
+            "Teología social y eclesiástica",
+            "Religión comparada y otras religiones",
+            "Ciencias sociales",
+            "Ciencia política",
+            "Economía",
+            "Administración pública y ciencia militar"});
+            this.comboBox2.Location = new System.Drawing.Point(143, 74);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(188, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Conocimiento",
+            "Sistemas",
+            "Procesamiento de datos Ciencia de los computadores",
+            "Programación, programas, datos de computador",
+            "Métodos especiales de computador",
+            "Bibliografías",
+            "Bibliotecología y ciencias de la información",
+            "Administración de personal",
+            "Operaciones bibliotecarias",
+            "Bibliotecas generales",
+            "Lectura y uso de otros medios de información",
+            "Medios noticiosos, periodísmo, publicación"});
+            this.comboBox3.Location = new System.Drawing.Point(477, 74);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(188, 21);
+            this.comboBox3.TabIndex = 10;
+            // 
+            // NuevoLibrocs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 466);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtemisor);
+            this.Controls.Add(this.label1);
+            this.Name = "NuevoLibrocs";
             this.Text = "NuevoLibrocs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtemisor;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
--- libro/libro/NuevoLibrocs.cs
+++ libro/libro/NuevoLibrocs.cs
@@ -16,5 +16,10 @@ namespace libro
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
