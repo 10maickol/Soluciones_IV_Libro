909431b365c1759e2bcbacd377ce789edb61228d
CarlosBernalA <57161416+CarlosBernalA@users.noreply.github.com>
2019-10-31 20:47:37 -0500

    nueva version

--- libro/libro/Libro.Designer.cs
+++ libro/libro/Libro.Designer.cs
@@ -28,19 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_openfile = new System.Windows.Forms.Button();
+            this.txt_archivoname = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(861, 156);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtros";
+            this.groupBox1.Enter += new System.EventHandler(this.GroupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 155);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(861, 325);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Lisbros";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txt_archivoname);
+            this.panel1.Controls.Add(this.btn_openfile);
+            this.panel1.Location = new System.Drawing.Point(12, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 112);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_openfile
+            // 
+            this.btn_openfile.Location = new System.Drawing.Point(27, 12);
+            this.btn_openfile.Name = "btn_openfile";
+            this.btn_openfile.Size = new System.Drawing.Size(132, 23);
+            this.btn_openfile.TabIndex = 0;
+            this.btn_openfile.Text = "Abrir Excel";
+            this.btn_openfile.UseVisualStyleBackColor = true;
+            // 
+            // txt_archivoname
+            // 
+            this.txt_archivoname.AutoSize = true;
+            this.txt_archivoname.Location = new System.Drawing.Point(24, 64);
+            this.txt_archivoname.Name = "txt_archivoname";
+            this.txt_archivoname.Size = new System.Drawing.Size(145, 13);
+            this.txt_archivoname.TabIndex = 1;
+            this.txt_archivoname.Text = "Ningun archivo seleccionado";
             // 
             // Libro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(861, 480);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Libro";
             this.Text = "Libro";
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label txt_archivoname;
+        private System.Windows.Forms.Button btn_openfile;
     }
 }
